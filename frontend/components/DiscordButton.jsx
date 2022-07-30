e04bdaab9a1e4517a274c48d5d6e34045b84f0bf
Mopsior <80514290+Mopsior@users.noreply.github.com>
2022-07-30 13:56:43 +0200

    Repair Discord href

--- frontend/components/DiscordButton.jsx
+++ frontend/components/DiscordButton.jsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export default function DiscordButton() {
     return (
         <Link href="https://discord.gg/bvyvz6aVsG" >
-            <a href="">
+            <a>
                 <div className={styles.button}>
                         <Image src="/Discord.png" alt='discord logo' width={30} height={"34px"} />
                         <p>WBIJAM</p>
